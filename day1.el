2a7153ab8cbdf5a7300a749844410565966c05f7
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-01-02 19:12:07 +0100

    day1: part1

---
+++ day1.el
@@ -0,0 +1,11 @@
+(defun smv/day1 ()
+  (interactive)
+  (let* (
+         (content (buffer-string))
+         (matches (s-match-strings-all "^\\([0-9]+\\) \\([0-9]+\\)" content))
+         (first-line (sort (mapcar (lambda (lst) (string-to-number (nth 1 lst))) matches) '<))
+         (second-line (sort (mapcar (lambda (lst) (string-to-number  (nth 2 lst))) matches) '<))
+         )
+    (message "%s" (seq-reduce #'+ (map 'list (lambda (x y) (if (< x y) (- y x) (- x y))) first-line second-line) 0))
+    )
+ )
