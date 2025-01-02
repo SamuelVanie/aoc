b4274980702a226db2124f32f11c8f9db3d880d7
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-01-02 20:01:07 +0100

    day1: part two of day1

--- day1.el
+++ day1.el
@@ -1,4 +1,4 @@
-(defun smv/day1 ()
+(defun smv/day1-part1 ()
   (interactive)
   (let* (
          (content (buffer-string))
@@ -9,3 +9,16 @@
     (message "%s" (seq-reduce #'+ (map 'list (lambda (x y) (if (< x y) (- y x) (- x y))) first-line second-line) 0))
     )
  )
+
+(defun smv/day1-part2 ()
+  "Calculate the similarity score"
+  (interactive)
+  (let* (
+         (content (buffer-string))
+         (matches (s-match-strings-all "^\\([0-9]+\\) \\([0-9]+\\)" content))
+         (first-line (mapcar (lambda (lst) (string-to-number (nth 1 lst))) matches))
+         (second-line (mapcar (lambda (lst) (string-to-number  (nth 2 lst))) matches))
+         )
+    (message "%s" (seq-reduce #'+ (map 'list (lambda (x) (* x (count x second-line))) first-line) 0))
+    )
+  )
