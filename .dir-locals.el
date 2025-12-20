a8a39830ce4b97f85997b7d83f98f69a24570965
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-12-20 15:05:11 -0500

    ci: add my cobol-ts-mode and a more robus init hook for find root dirs

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,6 +1,12 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
+;; some other command line parameters
+;; maybe necessary ?
+;; "-Dline.separator=\r\n",
+;; "-Dlogback.statusListenerClass=ch.qos.logback.core.status.NopStatusListener",
+;; "-DserverType=NATIVE",
+
 ((nil . ((eval . (progn
 		   (require 'lsp)
                    (add-to-list 'lsp-language-id-configuration '(cobol-ts-mode . "cobol"))
